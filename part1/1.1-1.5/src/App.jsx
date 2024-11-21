96264c3ed57883b6e8c1104e07bfcffa43a5e7b9
SERGIO CUEVAS ROMERO <sergiocuevas.romero.tech@bbva.com>
2024-11-21 08:01:29 +0100

    part1 - 1.5

--- part1/1.1-1.5/src/App.jsx
+++ part1/1.1-1.5/src/App.jsx
@@ -1,21 +1,23 @@
 import './App.css'
 
 function App() {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
 
   const Header = ({ course }) => {
@@ -52,9 +54,9 @@ function App() {
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
