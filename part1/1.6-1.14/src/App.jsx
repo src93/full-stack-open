00ef38ea1776bfc8fa8c00a7cec5f4e80d4b2e7c
SERGIO CUEVAS ROMERO <sergiocuevas.romero.tech@bbva.com>
2024-11-25 09:57:58 +0100

    part1 - 1.7

--- part1/1.6-1.14/src/App.jsx
+++ part1/1.6-1.14/src/App.jsx
@@ -2,13 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 const Title = () => (<h2>Give feedback</h2>)
-
 const Button = ({ onHandleClick, text }) => {
   return (
     <button onClick={onHandleClick}>{text}</button>
   )
 }
-
 const ContentButton = ({
   handleBadButton,
   handleNeutralButton,
@@ -22,28 +20,46 @@ const ContentButton = ({
     </div>
   )
 }
-
 const SecondTitle = () => (<h2>Statistics</h2>)
-
 const Statistic = ({ result, category }) => (<p>{category} {result}</p>)
 
 function App() {
   const [bad, setBad] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [good, setGood] = useState(0)
+  const [allComentaries, setAllComentaries] = useState(0)
+
+  const getAllComentaries = () => bad + neutral + good
+  const getAverage = () => (good * 1 + bad * -1) / allComentaries
+  const getAveragePositve = () => `${good / allComentaries * 100} %`
+  const handleOnClickBadButton = () => {
+    setBad(bad + 1)
+    setAllComentaries(allComentaries + 1)
+  }
+  const handleOnClickNeutralButton = () => {
+    setNeutral(neutral + 1)
+    setAllComentaries(allComentaries + 1)
+  }
+  const handleOnClickGoodButton = () => {
+    setGood(good + 1)
+    setAllComentaries(allComentaries + 1)
+  }
 
   return (
     <>
       <Title />
-      <ContentButton
-        handleBadButton={() => setBad(bad + 1)}
-        handleNeutralButton={() => setNeutral(neutral + 1)}
-        handleGoodButton={() => setGood(good + 1)}
-      />
+      <div className='content-button'>
+        <Button onHandleClick={handleOnClickBadButton} text={'Bad'} />
+        <Button onHandleClick={handleOnClickNeutralButton} text={'Neutral'} />
+        <Button onHandleClick={handleOnClickGoodButton} text={'Good'} />
+      </div>
       <SecondTitle />
       <Statistic category={'bad'} result={bad} />
       <Statistic category={'neutral'} result={neutral} />
       <Statistic category={'good'} result={good} />
+      <Statistic category={'all'} result={getAllComentaries()} />
+      <Statistic category={'average'} result={getAverage()} />
+      <Statistic category={'positive'} result={getAveragePositve()} />
     </>
   )
 }
