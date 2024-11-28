7c85f658b21b2ce364ad6e489b7a6f57013be8f1
SERGIO CUEVAS ROMERO <sergiocuevas.romero.tech@bbva.com>
2024-11-28 07:53:35 +0100

    part2 - 2.2

--- part2/2.1-2.6/src/App.jsx
+++ part2/2.1-2.6/src/App.jsx
@@ -3,6 +3,7 @@ const Course = ({ course }) => {
     <>
       <Header title={course.name} />
       <Content parts={course.parts} />
+      <TotalExercises total={course.parts.reduce((acc, current) => acc += current.exercises, 0)} />
     </>
   )
 }
@@ -12,6 +13,8 @@ const Header = ({ title }) => (<h1>{title}</h1>)
 const Content = ({ parts }) => parts.map(part => (<Part key={part.id} part={part} />))
 
 const Part = ({ part }) => (<p>{part.name} {part.exercises}</p>)
+
+const TotalExercises = ({ total }) => (<b>Total of {total} exercises</b>)
 
 const App = () => {
   const course = {
