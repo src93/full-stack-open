1de7d3ba9b4f21cdc17f672d21f66f9e2f48ae73
SERGIO CUEVAS ROMERO <sergiocuevas.romero.tech@bbva.com>
2025-02-15 20:04:15 +0100

    part4 - 4.4

--- part4/utils/list-helper.js
+++ part4/utils/list-helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (posts) => {
+  if (posts.length === 0) return 0;
+  return posts.reduce((acc, current) => {
+    return acc += current.likes
+  }, 0);
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
