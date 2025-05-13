1b4e537bd6afd8be87f16a5a2c011a59b67bce25
SERGIO CUEVAS ROMERO <sergiocuevas.romero.tech@bbva.com>
2025-05-13 20:37:07 +0200

    part6 - 6.7

--- part6/src/App.jsx
+++ part6/src/App.jsx
@@ -1,25 +1,38 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { votedAnecdote, createAnecdote } from './reducers/anecdoteReducer'
+import { votedAnecdote } from './reducers/anecdoteReducer'
+import { useState } from 'react'
+
+import Notification from './components/Notification'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
+  const [showNotification, setShowNotification] = useState(false)
+  const [notification, setNotification] = useState('')
 
   const vote = (id) => {
     console.log('vote', id)
     dispatch(votedAnecdote(id))
+    setNotification(`you voted '${anecdotes.find(a => a.id === id).content}'`)
+    setShowNotification(true)
+    setTimeout(() => {
+      setShowNotification(false)
+    }, 5000)
   }
 
-  const create = (event) => {
-    event.preventDefault()
-    const content = event.target.newNote.value
-    event.target.newNote.value = ''
-    dispatch(createAnecdote(content))
+  const handleSubmit = (content) => {
+    setNotification(`you created '${content}'`)
+    setShowNotification(true)
+    setTimeout(() => {
+      setShowNotification(false)
+    }, 5000)
   }
 
   return (
     <div>
       <h2>Anecdotes</h2>
+      {showNotification && <Notification notification={notification} />}
       {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
         <div key={anecdote.id}>
           <div>
@@ -31,11 +44,7 @@ const App = () => {
           </div>
         </div>
       )}
-      <h2>create new</h2>
-      <form onSubmit={create}>
-        <div><input name='newNote'/></div>
-        <button>create</button>
-      </form>
+      <AnecdoteForm handleSubmit={handleSubmit} />
     </div>
   )
 }
---
+++ part6/src/components/AnecdoteForm.jsx
@@ -0,0 +1,25 @@
+import { createAnecdote } from '../reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+
+const AnecdoteForm = ({ handleSubmit }) => {
+  const dispatch = useDispatch()
+  const create = (event) => {
+    event.preventDefault()
+    const content = event.target.newNote.value
+    event.target.newNote.value = ''
+    dispatch(createAnecdote(content))
+    handleSubmit(content)
+  }
+
+  return (
+    <>
+      <h2>create new</h2>
+      <form onSubmit={create}>
+        <div><input name='newNote'/></div>
+        <button>create</button>
+      </form>
+    </>
+  )
+}
+
+export default AnecdoteForm
