560e2ba7b6498508660f26a441468e8a3d91aa9b
SERGIO CUEVAS ROMERO <sergiocuevas.romero.tech@bbva.com>
2025-05-29 17:09:12 +0200

    part7 - 7.7

--- part7/src/App.jsx
+++ part7/src/App.jsx
@@ -1,169 +1,84 @@
-import { useState } from 'react'
-import { Route, Routes, Link, useMatch, useNavigate } from 'react-router'
-import { useFiled } from './hooks'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const Menu = () => {
-  const padding = {
-    paddingRight: 5
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
   }
-  return (
-    <div>
-      <Link to={'/'} style={padding}>anecdotes</Link>
-      <Link to={'/create'} style={padding}>create new</Link>
-      <Link to={'/about'} style={padding}>about</Link>
-    </div>
-  )
+
+  return {
+    type,
+    value,
+    onChange
+  }
 }
 
-const AnecdoteList = ({ anecdotes }) => (
-  <div>
-    <h2>Anecdotes</h2>
-    <ul>
-      {anecdotes.map(anecdote => {
-        return (
-          <li key={anecdote.id}>
-            <Link to={`anecdote/${anecdote.id}`} >{anecdote.content}</Link>
-          </li>
-        )
-      })}
-    </ul>
-  </div>
-)
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
 
-const About = () => (
-  <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
+  useEffect(() => {
+    if (!name) return
+    axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+      .then(res => {
+        setCountry({
+          found: true,
+          data: res.data
+        })
+      })
+      .catch(() => {
+        setCountry({
+          found: false,
+          data: null
+        })
+      })
+  }, [name])
 
-    <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is "a story with a point."</em>
+  return country
+}
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
-  </div>
-)
-
-const Footer = () => (
-  <div>
-    Anecdote app for <a href='https://fullstackopen.com/'>Full Stack Open</a>.
-
-    See <a href='https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js</a> for the source code.
-  </div>
-)
-
-const CreateNew = (props) => {
-  const {reset: resetContent, ...content} = useFiled('content')
-  const {reset: resetAuthor, ...author} = useFiled('author')
-  const {reset: resetInfo, ...info} = useFiled('info')
-  const navigate = useNavigate()
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    props.addNew({
-      content: content.value,
-      author: author.value,
-      info: info.value,
-      votes: 0
-    })
-    navigate('/')
+const Country = ({ ...country }) => {
+  if (!country) {
+    return null
   }
 
-  const handleReset = () => {
-    resetContent()
-    resetAuthor()
-    resetInfo()
+  if (!country.found) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
   }
 
   return (
     <div>
-      <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          content
-          <input { ...content } />
-        </div>
-        <div>
-          author
-          <input { ...author } />
-        </div>
-        <div>
-          url for more info
-          <input { ...info } />
-        </div>
-        <button>create</button>
-      </form>
-      <button onClick={handleReset}>reset</button>
+      <h3>{country.data.name.common} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div> 
+      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name.common}`}/>  
     </div>
-  )
-}
-
-const Anecdote = ({ anecdote }) => {
-  // const id = parseInt(useParams().id)
-  return (
-    <>
-      <h2>{anecdote.content}</h2>
-      <p>by {anecdote.author}</p>
-      <p>has {anecdote.votes} votes</p>
-    </>
   )
 }
 
 const App = () => {
-  const [anecdotes, setAnecdotes] = useState([
-    {
-      content: 'If it hurts, do it more often',
-      author: 'Jez Humble',
-      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
-      votes: 0,
-      id: 1
-    },
-    {
-      content: 'Premature optimization is the root of all evil',
-      author: 'Donald Knuth',
-      info: 'http://wiki.c2.com/?PrematureOptimization',
-      votes: 0,
-      id: 2
-    }
-  ])
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
 
-  const [notification, setNotification] = useState('')
-  const matchAnecdoteId = useMatch('/anecdote/:id')
-  const anecdote = anecdotes.find(a => a.id === Number(matchAnecdoteId?.params.id))
-
-  const addNew = (anecdote) => {
-    anecdote.id = Math.round(Math.random() * 10000)
-    setAnecdotes(anecdotes.concat(anecdote))
-    setNotification(`a new anecdote ${anecdote.content} created!`)
-    setTimeout(() => setNotification(''), 5000)
-  }
-
-  const anecdoteById = (id) =>
-    anecdotes.find(a => a.id === id)
-
-  const vote = (id) => {
-    const anecdote = anecdoteById(id)
-
-    const voted = {
-      ...anecdote,
-      votes: anecdote.votes + 1
-    }
-
-    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
   }
 
   return (
     <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      {notification && <p>{notification}</p> || ''}
-      <Routes>
-        <Route path='/' element={ <AnecdoteList anecdotes={anecdotes} /> } />
-        <Route path='/about' element={ <About /> } />
-        <Route path='/create' element={ <CreateNew addNew={addNew} /> } />
-        <Route path='/anecdote/:id' element={ <Anecdote anecdote={anecdote} /> } />
-      </Routes>
-      <Footer />
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country {...country} />
     </div>
   )
 }
