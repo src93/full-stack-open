2133e7c1757f3007c44424dd65f36ce2c9636b29
SERGIO CUEVAS ROMERO <sergiocuevas.romero.tech@bbva.com>
2025-07-08 12:44:48 +0200

    part8 - 8.4

--- part8/backend/serverGQL.js
+++ part8/backend/serverGQL.js
@@ -116,7 +116,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -125,7 +125,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) return books
+      const booksByAuthor = books.filter(book => book.author === args.author)
+      return booksByAuthor
+    },
     allAuthors: () => authors
   },
   Author: {
